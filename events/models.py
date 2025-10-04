f8af48afd8dd863615f626c42403fe239fc13be4
yashchandak101 <chandak.yash101@gmail.com>
2025-10-04 07:12:13 +0530

    Update models.py

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
@@ -6,7 +7,7 @@ class Event(models.Model):
     description = models.TextField()
     date = models.DateTimeField()
     location = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="events/", blank=True, null=True)
+    image = CloudinaryField("image", blank=True, null=True))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
