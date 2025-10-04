98487fac550e7ab6baf871f6d72ba281aa76b8b5
yashchandak101 <chandak.yash101@gmail.com>
2025-10-04 05:37:06 +0530

    Update urls.py

--- crappylife/urls.py
+++ crappylife/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from django.conf import settings
@@ -35,10 +35,10 @@ urlpatterns = [
     path("api/newsletter/", include("newsletter.urls")),
     path("api/analytics/", include("analytics.urls")),
     path("api/notifications/", include("notifications.urls")),
-    path("media/<path:path>", serve, {"document_root": settings.MEDIA_ROOT}),
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+if settings.MEDIA_ROOT:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
