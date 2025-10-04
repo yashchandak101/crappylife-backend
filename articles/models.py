a4271b12f2977228180d0424280490f27e4ad019
yashchandak101 <chandak.yash101@gmail.com>
2025-10-04 07:13:04 +0530

    Update models.py

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
 User = settings.AUTH_USER_MODEL
 
