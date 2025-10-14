cbc7c74f24bf4b394e0186c6d7ff19a1e06ca76a
Yash Chandak <chandak.yash101@gmail.com>
2025-10-15 00:13:10 +0530

    change

--- accounts/apps.py
+++ accounts/apps.py
@@ -5,4 +5,7 @@ class AccountsConfig(AppConfig):
     name = "accounts"
 
     def ready(self):
-        import accounts.signals  # <-- just import signals here
+        from django.db.models.signals import post_migrate
+        from accounts import signals  # Lazy import
+
+        post_migrate.connect(signals.create_default_roles, dispatch_uid="create_default_roles_safe")
