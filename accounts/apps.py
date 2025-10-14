42b6c2b39207d0a57a53604db960c36d2243637c
Yash Chandak <chandak.yash101@gmail.com>
2025-10-14 21:14:19 +0530

    c

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,33 +1,47 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accounts"
 
     def ready(self):
         from django.contrib.auth.models import Group, Permission
         from django.contrib.contenttypes.models import ContentType
-        from articles.models import Article
+        from django.db.utils import OperationalError, ProgrammingError
+        from django.dispatch import receiver
 
-        # Create roles only if not already created
-        if not Group.objects.filter(name="Author").exists():
-            author_group, _ = Group.objects.get_or_create(name="Author")
-            author_perms = Permission.objects.filter(
-                content_type=ContentType.objects.get_for_model(Article),
-                codename__in=["add_article", "change_article", "view_article"]
-            )
-            author_group.permissions.set(author_perms)
-            author_group.save()
+        @receiver(post_migrate)
+        def create_default_roles(sender, **kwargs):
+            try:
+                from articles.models import Article  # import inside to avoid early import issues
 
-            editor_group, _ = Group.objects.get_or_create(name="Editor")
-            editor_perms = Permission.objects.filter(
-                content_type=ContentType.objects.get_for_model(Article),
-                codename__in=["change_article", "view_article", "delete_article"]
-            )
-            editor_group.permissions.set(editor_perms)
-            editor_group.save()
+                # Author
+                author_group, _ = Group.objects.get_or_create(name="Author")
+                author_perms = Permission.objects.filter(
+                    content_type=ContentType.objects.get_for_model(Article),
+                    codename__in=["add_article", "change_article", "view_article"]
+                )
+                author_group.permissions.set(author_perms)
+                author_group.save()
 
-            admin_group, _ = Group.objects.get_or_create(name="Admin")
-            admin_group.permissions.set(Permission.objects.all())
-            admin_group.save()
+                # Editor
+                editor_group, _ = Group.objects.get_or_create(name="Editor")
+                editor_perms = Permission.objects.filter(
+                    content_type=ContentType.objects.get_for_model(Article),
+                    codename__in=["change_article", "view_article", "delete_article"]
+                )
+                editor_group.permissions.set(editor_perms)
+                editor_group.save()
+
+                # Admin
+                admin_group, _ = Group.objects.get_or_create(name="Admin")
+                admin_group.permissions.set(Permission.objects.all())
+                admin_group.save()
+
+                print("✅ Default roles created successfully!")
+
+            except (OperationalError, ProgrammingError):
+                # Happens if migrations not done yet
+                pass
